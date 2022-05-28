cbbea3b14db2042fec573682a6ef2c60356d4147
yechanpark <qkrdpcks0419@naver.com>
2022-05-28 10:14:16 +0900

    initial commit

---
+++ src/main/kotlin/com/example/springkafkatest/SpringkafkatestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkafkatest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringkafkatestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringkafkatestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springkafkatest/SpringkafkatestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springkafkatest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringkafkatestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
